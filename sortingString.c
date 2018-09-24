d52f2541706ad8b822226241061cc917c5e87dd8
kunallguptaaa <kunallguptaa@gmail.com>
2018-09-24 17:40:42 +0530

    Biggenning code

--- sortingString.c
+++ sortingString.c
@@ -4,5 +4,13 @@
 
 void main()
 {
-    
+  int n = 0;
+  printf("Enter the size of the array of strings");
+  scanf("%d", &n);
+  char *ptr = malloc(n*sizeof());
+  for(int i  = 0; i < n; i++)
+  {
+
+  }
+
 }
